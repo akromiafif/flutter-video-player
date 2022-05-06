3ea0fbc7e5ee091af3d2748e299e9cef17f08120
Afif Akromi <massachusetts48@gmail.com>
2022-05-07 00:16:00 +0700

    fix second banner

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -173,6 +173,76 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              height: 180,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(top: 30),
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage("assets/card.jpg"),
+                            fit: BoxFit.fill),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(8, 10),
+                              color: colors.AppColor.gradientSecond
+                                  .withOpacity(0.3)),
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(-1, -5),
+                              color: colors.AppColor.gradientSecond
+                                  .withOpacity(0.3))
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(right: 200, bottom: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          image: AssetImage("assets/figure.png")),
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    margin: const EdgeInsets.only(left: 150, top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("You are doing great",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: colors.AppColor.homePageDetail)),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                text: "Keep it up \n",
+                                style: TextStyle(
+                                    color: colors.AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                                children: [
+                              TextSpan(text: "Stick to your plan")
+                            ])),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
